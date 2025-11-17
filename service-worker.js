e378411a5c650afd40d321df659c45dc708077ab
bbawaliphy-lgtm <bbawaliphy@gmail.com>
2025-11-17 20:18:36 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "lipomix-cache-v1";
+const URLS_TO_CACHE = [
+    "./",
+    "./lipid_calculator.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+// Install - Cache all essential files
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(URLS_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+// Fetch - Try cache first, then network
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Activate - Remove old caches
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+});
